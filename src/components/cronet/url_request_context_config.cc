3e13ae80d6bd7fcd0b9cf8f73dc91c84c8f00a74
HunterHiy <hunterwishiy@gmail.com>
2026-05-05 11:53:19 +0100

    Remove error directive in url_request_context_config.cc
    Remove error directive for PATCH_MARKER_CRONET_PROXY_TEST.

--- src/components/cronet/url_request_context_config.cc
+++ src/components/cronet/url_request_context_config.cc
@@ -1,7 +1,6 @@
 // Copyright 2014 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-#error "PATCH_MARKER_CRONET_PROXY_TEST"
 
 #include "components/cronet/url_request_context_config.h"
 
